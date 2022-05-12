97f0b6af85fd0b084ba1bcb4e26ade41c2dbd08b
Your Name <you@example.com>
2022-05-12 11:13:13 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -4,7 +4,7 @@
 
 
 typedef struct{
-   int sudo[9][9];
+  int sudo[9][9];
 }Node;
 
 Node* createNode(){
@@ -13,9 +13,9 @@ Node* createNode(){
 }
 
 Node* copy(Node* n){
-    Node* new=(Node*) malloc(sizeof(Node));
-    *new = *n;
-    return new;
+  Node* new=(Node*) malloc(sizeof(Node));
+  *new = *n;
+  return new;
 }
 
 Node* read_file (char* file_name){
@@ -23,10 +23,10 @@ Node* read_file (char* file_name){
   FILE* file = fopen (file_name, "r");
   int i,j;
   for(i=0;i<9;i++){
-       for(j=0;j<9;j++){
-          if(!fscanf (file, "%d", &n->sudo[i][j]))
-            printf("failed to read data!");
-       }
+    for(j=0;j<9;j++){
+      if(!fscanf (file, "%d", &n->sudo[i][j]))
+      {printf("failed to read data!");}
+    }
   }
 
   fclose (file);
@@ -34,29 +34,32 @@ Node* read_file (char* file_name){
 }
 
 void print_node(Node* n){
-    int i,j;
-    for(i=0;i<9;i++){
-       for(j=0;j<9;j++)
-          printf("%d ", n->sudo[i][j]);
-       printf("\n");
-    }
+  int i,j;
+  for(i=0;i<9;i++){
+    for(j=0;j<9;j++)
+      printf("%d ", n->sudo[i][j]);
     printf("\n");
+  }
+  printf("\n");
 }
 
 int is_valid(Node* n){
 
-    return 1;
+  return 1;
 }
 
 
 List* get_adj_nodes(Node* n){
-    List* list=createList();
-    return list;
+  List* list=createList();
+  
+
+  
+  return list;
 }
 
 
 int is_final(Node* n){
-    return 0;
+  return 0;
 }
 
 Node* DFS(Node* initial, int* cont){
