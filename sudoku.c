81b24c3aa889df7c3976910dc9a7deb682891c9b
Your Name <you@example.com>
2022-05-12 21:09:51 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -66,10 +66,10 @@ int is_valid(Node* n){
   {
     for (size_t j = 0; j < 9; j++)
     {
-      if (n->sudo[i][j] != 0)
+      if (n->sudo[j][i] != 0)
       {
-        vectorNum[n->sudo[i][j]]++;
-        if (vectorNum[n->sudo[i][j]] > 1){return 0;}
+        vectorNum[n->sudo[j][i]]++;
+        if (vectorNum[n->sudo[j][i]] > 1){return 0;}
       }
     }
     free(vectorNum);
